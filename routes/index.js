0e3bb18773fc51ebdd4f9a406dbf45c5d3625dfd
ohmpatell <ohm2003.patel@gmail.com>
2023-09-24 19:03:57 -0400

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+*/
+
+router.get("/", function (req, res, next) {
+  res.render("home", { title: "Home" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" });
+});
+
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+
+router.get("/projects", function (req, res, next) {
+  res.render("projects", { title: "Projects" });
 });
 
 module.exports = router;
